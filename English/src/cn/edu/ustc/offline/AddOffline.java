f758db815018e91c3c7af7c6c28612bf1eae61df
hushuaigithub <997082715@qq.com>
2014-11-05 21:58:26 +0800

    20141105-1

---
+++ English/src/cn/edu/ustc/offline/AddOffline.java
@@ -0,0 +1,74 @@
+package cn.edu.ustc.offline;
+
+import cn.edu.ustc.main.FileOperate;
+import cn.edu.ustc.main.R;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class AddOffline extends Activity {
+
+	private String[] fileNames;
+	private File[] currentFiles;
+	private File currentParent;
+	private ListView sdcardList;
+	
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.offline_add);
+    	sdcardList = (ListView) findViewById(R.id.showlistoffline);
+       
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+        	File f = new File("/mnt/sdcard/");
+        	currentParent=f;
+        	currentFiles = f.listFiles(); 
+        	showListFiles(currentFiles);
+        }
+        else{
+        	Toast.makeText(this,"SD¿¨²»´æÔÚ£¡",Toast.LENGTH_SHORT ).show();
+        }
+        
+    }
+      
+    private void showListFiles(File[] file){   	
+    	
+	}
+    
+    private List<Map<String, Object>> getData() {  
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();                
+                
+        return list;  
+    } 
+    
+	class Add_File {
+		public Add_File(File soucrfile,File targetfile){	
+			
+		}
+	}
+	
+	public void onListItemClick(final int position) {
+			
+	}
+	
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		return super.onKeyDown(keyCode, event);
+	}
+}
---
+++ English/src/cn/edu/ustc/offline/BookPageFactory.java
@@ -0,0 +1,129 @@
+
+package cn.edu.ustc.offline;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.text.DecimalFormat;
+import java.util.Vector;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+
+public class BookPageFactory {
+
+	private File book_file = null;
+	private MappedByteBuffer m_mbBuf = null;
+	private int m_mbBufLen = 0;
+	private int m_mbBufBegin = 0;
+	private int m_mbBufEnd = 0;
+	private String m_strCharsetName = "GBK";
+	private Bitmap m_book_bg = null;
+	private int mWidth;
+	private int mHeight;
+
+	private Vector<String> m_lines = new Vector<String>();
+
+	private int m_fontSize = 24;
+	private int m_textColor = Color.BLACK;
+	private int m_backColor = 0xffff9e85; // 背景颜色
+	private int marginWidth = 15; // 左右与边缘的距离
+	private int marginHeight = 20; // 上下与边缘的距离
+
+	private int mLineCount; // 每页可以显示的行数
+	private float mVisibleHeight; // 绘制内容的宽
+	private float mVisibleWidth; // 绘制内容的宽
+	private boolean m_isfirstPage,m_islastPage;
+
+	// private int m_nLineSpaceing = 5;
+
+	private Paint mPaint;
+
+	public BookPageFactory(int w, int h) {
+		// TODO Auto-generated constructor stub
+		mWidth = w;
+		mHeight = h;
+		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		mPaint.setTextAlign(Align.LEFT);
+		mPaint.setTextSize(m_fontSize);
+		mPaint.setColor(m_textColor);
+		mVisibleWidth = mWidth - marginWidth * 2;
+		mVisibleHeight = mHeight - marginHeight * 2;
+		mLineCount = (int) (mVisibleHeight / m_fontSize); // 可显示的行数
+	}
+
+	public void openbook(String strFilePath) throws IOException {
+		book_file = new File(strFilePath);
+		long lLen = book_file.length();
+		m_mbBufLen = (int) lLen;
+		m_mbBuf = new RandomAccessFile(book_file, "r").getChannel().map(
+				FileChannel.MapMode.READ_ONLY, 0, lLen);
+	}
+	
+
+	protected byte[] readParagraphBack(int nFromPos) {
+		int nEnd = nFromPos;
+		int i;
+		byte b0, b1;
+		if (m_strCharsetName.equals("UTF-16LE")) {
+			i = nEnd - 2;
+			while (i > 0) {
+				b0 = m_mbBuf.get(i);
+				b1 = m_mbBuf.get(i + 1);
+				if (b0 == 0x0a && b1 == 0x00 && i != nEnd - 2) {
+					i += 2;
+					break;
+				}
+				i--;
+			}
+
+		} else if (m_strCharsetName.equals("UTF-16BE")) {
+			i = nEnd - 2;
+			while (i > 0) {
+				b0 = m_mbBuf.get(i);
+				b1 = m_mbBuf.get(i + 1);
+				if (b0 == 0x00 && b1 == 0x0a && i != nEnd - 2) {
+					i += 2;
+					break;
+				}
+				i--;
+			}
+		} else {
+			i = nEnd - 1;
+			while (i > 0) {
+				b0 = m_mbBuf.get(i);
+				if (b0 == 0x0a && i != nEnd - 1) {
+					i++;
+					break;
+				}
+				i--;
+			}
+		}
+		if (i < 0)
+			i = 0;
+		int nParaSize = nEnd - i;
+		int j;
+		byte[] buf = new byte[nParaSize];
+		for (j = 0; j < nParaSize; j++) {
+			buf[j] = m_mbBuf.get(i + j);
+		}
+		return buf;
+	}
+
+	public void setBgBitmap(Bitmap BG) {
+		m_book_bg = BG;
+	}
+	
+	public boolean isfirstPage() {
+		return m_isfirstPage;
+	}
+	public boolean islastPage() {
+		return m_islastPage;
+	}
+}
