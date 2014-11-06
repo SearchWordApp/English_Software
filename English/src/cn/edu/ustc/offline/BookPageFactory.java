5d914a8975ed3baf2261e2ca22621d247b054ece
hushuaigithub <997082715@qq.com>
2014-11-06 18:48:27 +0800

    Revert "20141106-7"
    This reverts commit 4a5ff6683b800168dde6b76784a081429902afb3.

--- English/src/cn/edu/ustc/offline/BookPageFactory.java
+++ English/src/cn/edu/ustc/offline/BookPageFactory.java
@@ -149,26 +149,6 @@ public class BookPageFactory {
 					break;
 				}
 			}
-		}else if (m_strCharsetName.equals("UTF-16BE")) {
-			while (i < m_mbBufLen - 1) {
-				b0 = m_mbBuf.get(i++);
-				b1 = m_mbBuf.get(i++);
-				if (b0 == 0x00 && b1 == 0x0a) {
-					break;
-				}
-			}
-		} else {
-			while (i < m_mbBufLen) {
-				b0 = m_mbBuf.get(i++);
-				if (b0 == 0x0a) {
-					break;
-				}
-			}
-		}
-		int nParaSize = i - nStart;
-		byte[] buf = new byte[nParaSize];
-		for (i = 0; i < nParaSize; i++) {
-			buf[i] = m_mbBuf.get(nFromPos + i);
 		} 
 		return buf;
 	}
