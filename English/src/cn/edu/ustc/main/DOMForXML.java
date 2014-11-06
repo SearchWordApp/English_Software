5f7c0fdd5e149ccf1d71f884f0aae8ff394deeeb
SSEgl <Gandroid@126.com>
2014-11-06 20:53:36 +0800

    20141106-2

--- English/src/cn/edu/ustc/main/DOMForXML.java
+++ English/src/cn/edu/ustc/main/DOMForXML.java
@@ -17,14 +17,6 @@ import org.w3c.dom.NodeList;
 
 
 public class DOMForXML {
-		/*<book>
-			<item>    
-				<word>cupboard</word>
-				<trans><![CDATA[n. 食橱；碗柜]]></trans>
-				<phonetic><![CDATA[['kʌbəd]]]></phonetic>
-				<tags>CET4-EASY</tags>
-			</item>
-		</book>*/
 	public void addNewNode(String wordAdd,String transAdd, String phonAdd, String tagsAdd, String path){		
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();        
@@ -48,7 +40,6 @@ public class DOMForXML {
 			item.appendChild(tags);
 			root.appendChild(item);
 						
-			 // 此时真正的处理将新数据添加到文件中 磁盘
 			TransformerFactory tf = TransformerFactory.newInstance();
 			Transformer tfer = tf.newTransformer();
 			DOMSource ds = new DOMSource(document);
@@ -59,61 +50,3 @@ public class DOMForXML {
 		}		
 	}
 	
-	public void delNode(String wordDel, String path) {
-		try {
-			DocumentBuilderFactory factory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document = builder.parse(new File(path));
-			NodeList list = document.getElementsByTagName("word");  
-			for (int i = 0; i < list.getLength(); i++) {
-				String value = list.item(i).getFirstChild()  
-                        .getTextContent();  
-                if (wordDel != null && wordDel.equalsIgnoreCase(value)) {  
-                    Node parentNode = list.item(i).getParentNode();  
-                    document.getFirstChild().removeChild(parentNode);  
-                    break;
-                }  
-			}
-			TransformerFactory tsf = TransformerFactory.newInstance();
-			Transformer tf = tsf.newTransformer();
-			StreamResult result = new StreamResult(new File(path));
-			DOMSource source = new DOMSource(document);
-			tf.transform(source, result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void updateNode(String word, String trans, String path){
-		try{
-			DocumentBuilderFactory factory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document = builder.parse(new File(path));
-			NodeList list = document.getElementsByTagName("word");  
-			for (int i = 0; i < list.getLength(); i++) {  
-                String value = list.item(i).getTextContent();  
-                if (word != null && word.equalsIgnoreCase(value)) {  
-                    Node parentNode = list.item(i).getParentNode();  
-                    NodeList nl = parentNode.getChildNodes();  
-                    for (int j = 0; j < nl.getLength(); j++) {  
-                        String modifyNode = nl.item(j).getNodeName();  
-                        if (modifyNode.equalsIgnoreCase("trans")) {  
-                            nl.item(j).setTextContent(trans);  
-                            break;
-                        }  
-                    }  
-                    break;
-                }  
-            }  	
-			TransformerFactory tsf = TransformerFactory.newInstance();
-			Transformer tf = tsf.newTransformer();
-			StreamResult result = new StreamResult(new File(path));
-			DOMSource source = new DOMSource(document);
-			tf.transform(source, result);
-		} catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-}
