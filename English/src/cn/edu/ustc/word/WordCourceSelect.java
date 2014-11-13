071380c1b4989c464beb642d9e6d6d64b75dee1b
hushuaigithub <997082715@qq.com>
2014-11-13 22:19:04 +0800

    20141113-1

--- English/src/cn/edu/ustc/word/WordCourceSelect.java
+++ English/src/cn/edu/ustc/word/WordCourceSelect.java
@@ -51,7 +51,57 @@ public class WordCourceSelect extends Activity {
 				handler.sendMessage(msg);
 			}
 		}.start();
+		
+		Button button_minus = (Button) this.findViewById(R.id.cource_group)
+				.findViewById(R.id.button_cource_minus);
+		Button button_add = (Button) findViewById(R.id.button_cource_add);
+		button_add.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (wordCount != 0) {
+					cource = Integer.valueOf(editText.getText().toString());
+					if (cource < courceCount)
+						cource++;
+					editText.setText(String.valueOf(cource));
+				}
+			}
+		});
 
+		button_minus.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (wordCount != 0) {
+					cource = Integer.valueOf(editText.getText().toString());
+					if (cource > 1)
+						cource--;
+					editText.setText(String.valueOf(cource));
+				}
+			}
+		});
+		
+		Button button_startButton = (Button) findViewById(R.id.button_word_learn_start);
+		button_startButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (wordCount == 0) {
+					Toast.makeText(WordCourceSelect.this, "当前词库没有单词！",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					try{
+						cource = Integer.parseInt(editText.getText().toString().trim());
+					}catch (Exception e){
+						cource = 1;
+					}
+					Intent intent = new Intent();
+					Bundle bundle = new Bundle();
+					bundle.putInt("courceSize", courceSize);
+					bundle.putInt("cource", cource);
+					bundle.putInt("wordCount", wordCount);
+					bundle.putString("filename", filename);
+					intent.putExtras(bundle);
+					intent.setClass(WordCourceSelect.this, WordLearnShow.class);
+					startActivity(intent);
+				}
+			}
+		});
+	}
 
 		
 }
